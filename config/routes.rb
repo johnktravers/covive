316a1db0cba1faa3deadd5d0443e7c95929d6918
John Travers <johnk.travers@gmail.com>
2020-03-29 23:30:14 -0600

    Add welcome controller with index action and route using react router

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  match '*path', to: 'welcome#index', via: :all
 end
